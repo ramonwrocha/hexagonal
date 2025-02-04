38665664f571f2245953b3bc0400624b04fca3a5
ramon-w-rocha <ramon.w.rocha@outlook.com>
2025-02-05 00:03:10 +0100

    EF

---
+++ BookingService/Adapters/Data/DataAccess/HotelBookingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.DataAccess;
+
+public class HotelBookingDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<RoomEntity> Rooms { get; set; }
+
+    public DbSet<GuestEntity> Guests { get; set; }
+
+    public DbSet<BookingEntity> Bookings { get; set; }
+}
--- BookingService/Consumers/Api/Program.cs
+++ BookingService/Consumers/Api/Program.cs
@@ -1,8 +1,21 @@
+using Data.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+#region DB PostGres TODO:Create IoC
+var connectionStringPostgres = builder.Configuration.GetValue<string>("ConnectionString:PostgresConnection");
+builder.Services.AddDbContext<HotelBookingDbContext>(dbContextOptions =>
+{
+    dbContextOptions.UseNpgsql(connectionStringPostgres);
+});
+#endregion
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
